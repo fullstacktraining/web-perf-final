3414f20125af52979b86b85b9cd8546481a37ffc
Tamas Piros <tpiros@tpiros-macbook.local>
2019-02-08 11:01:45 +0100

    updates

--- server.js
+++ server.js
@@ -1,36 +1,13 @@
-// const express = require('express');
-// const app = express();
-// const history = require('connect-history-api-fallback');
-// const compression = require('compression');
-// const port = 80;
-// app.use(compression());
-// app.use(express.static(`${__dirname}/dist`));
-// // app.use(history({
-// //   verbose: true,
-// //   disableDotRule: true
-// // }));
-// // app.use(express.static(`${__dirname}/dist`));
-// app.use((req, res, next) => {
-//   if (!req.originalUrl.includes('/dist/', 0)) {
-//     res.sendFile(`${__dirname}/dist/index.html`);
-//   } else {
-//     next();
-//   }
-// });
-
-// app.get('/', (req, res) => {
-//   return res.render(`${__dirname}/dist/index.html`)
-// });
-
-// app.listen(port, () => console.info(`HTTP server up on port ${port}`));
-
-
 const express = require('express');
 const app = express();
-const history = require('connect-history-api-fallback');
 const port = 80;
-
-app.use(history());
-app.use(express.static('dist'));
-
-app.listen(port, () => console.info(`HTTP server up on port ${port}`));
+const buildLocation = 'dist';
+app.use(express.static(buildLocation));
+app.use((req, res, next) => {
+  if (!req.originalUrl.includes(buildLocation)) {
+    res.sendFile(`${__dirname}/${buildLocation}/index.html`);
+  } else {
+    next();
+  }
+});
+app.listen(port, () => console.info(`Server running on port ${port}`));
--- src/service-worker.js
+++ src/service-worker.js
@@ -9,7 +9,7 @@ const cloudinaryPlugin = {
       const format = 'f_auto';
       switch ((navigator && navigator.connection) ? navigator.connection.effectiveType : '') {
         case '4g':
-          newPart = 'q_auto:good'; // q_auto === q_auto:good
+          newPart = 'q_auto:good';
         break;
   
         case '3g':
@@ -33,21 +33,19 @@ const cloudinaryPlugin = {
   }
 };
 
-const cloudinaryVideoCapturePlugin = {
-  requestWillFetch: async ({ request }) => {
-    console.log('video image caching');
-    if (/\.jpg$/.test(request.url)) {
-      const url = request.url.split('/');
-      const insertIndex = url.indexOf('upload');
-      const format = 'f_auto';
-      const quality = 'q_auto';
-      url.splice(insertIndex + 1, 0, `${quality},${format}`);
-      const finalUrl = url.join('/');
-      const newUrl = new URL(finalUrl);
-      return new Request(newUrl.href, { headers: request.headers });
-    }
-  }
-};
+workbox.routing.registerRoute(
+  new RegExp('^https:\/\/res\.cloudinary\.com\/.*\/image\/upload\/'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'cloudinary-images',
+    plugins: [
+      cloudinaryPlugin,
+      new workbox.expiration.Plugin({
+        maxEntries: 50,
+        purgeOnQuotaError: true,
+      }),
+    ],
+  })
+);
 
 workbox.routing.registerRoute(
   new RegExp('^https:\/\/res\.cloudinary\.com\/.*\/video\/upload\/.+[j][p][g]'),
